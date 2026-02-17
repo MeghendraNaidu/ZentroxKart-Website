45c26778b1cbe746828327144d2581a2752913d5
MeghendraNaidu <meghendranarravula@Gmail.com>
2026-02-18 00:01:27 +0530

    Cart Updated things is Add to cart, Remove Cart items

--- carts/views.py
+++ carts/views.py
@@ -39,22 +39,42 @@ def add_cart(request, product_id):
         cart_item.save()
     # return HttpResponse (cart_item.quantity)    
     return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product, cart = cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    product = get_object_or_404(Product, id = product_id)
+    cart_item = CartItem.objects.get(product = product, cart = cart)
+    cart_item.delete()
+    return redirect('cart')
          
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        # tax = 0
-        # grand_total = 0
+        tax = 0
+        grand_total = 0
         # if request.user.is_authenticated:
         #     cart_items = CartItem.objects.filter(user=request.user, is_active=True)
         # else:
         cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        cart_items = CartItem.objects.filter(cart=cart)  #Chat GPT
+        # cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
-        # tax = (2 * total)/100
-        # grand_total = total + tax
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectDoesNotExist:
         pass #just ignore
 
@@ -62,7 +82,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        # 'tax'       : tax,
-        # 'grand_total': grand_total,
+        'tax'       : tax,
+        'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
+    # return HttpResponse(cart_items)
