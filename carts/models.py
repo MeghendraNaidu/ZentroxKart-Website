0f9450bb3cda002585d153fee01a2cb0ee80e48f
MeghendraNaidu <meghendranarravula@Gmail.com>
2026-02-18 22:36:36 +0530

    store and carts functionality

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from store.models import Product #, Variation
+from store.models import Product, Variation
 # from accounts.models import Account
 
 
@@ -16,13 +16,13 @@ class Cart(models.Model):
 class CartItem(models.Model):
     # user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # variations = models.ManyToManyField(Variation, blank=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
     
-    def __str__(self):
-        return self.product
+    # def __str__(self):
+    #     return self.product
     
     # def __str__(self):
     #     return self.product.product_name
@@ -30,5 +30,5 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
-    # def __unicode__(self):
-    #     return self.product
+    def __unicode__(self):
+        return self.product
