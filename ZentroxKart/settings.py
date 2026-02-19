76f17722be641a38e6ab5479b2f69a237661bbb9
MeghendraNaidu <meghendranarravula@Gmail.com>
2026-02-19 21:44:22 +0530

    App Authentication

--- ZentroxKart/settings.py
+++ ZentroxKart/settings.py
@@ -155,3 +155,23 @@ MEDIA_ROOT = BASE_DIR /'media'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+# SMTP configuration
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+# EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_PORT = os.getenv('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', cast=bool)
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cellgamecell79@gmail.com'
+EMAIL_HOST_PASSWORD = 'kraxeodujvaktyrx'
+EMAIL_USE_TLS = True
